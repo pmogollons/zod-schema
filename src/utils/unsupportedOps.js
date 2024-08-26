0045917a30fbfa8bbd2c1b6f20b67a519855fc1c
Paulo Mogollón <pmogollon@unbosque.edu.co>
2024-08-25 22:15:23 -0500

    Added more unsupportedOps

--- src/utils/unsupportedOps.js
+++ src/utils/unsupportedOps.js
@@ -1,9 +1,16 @@
 export const unsupportedOps = [
+  "$inc",
+  "$mul",
+  "$min",
+  "$max",
   "$unset",
   "$rename",
   "$currentDate",
 
+  "$",
+  "$[]",
   "$pop",
   "$pull",
   "$pullAll",
+  "$bit",
 ];
