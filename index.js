23c3a0f4be981a5b8dbef7708a70a1ffea538b9a
Paulo Mogollon <pmogollon@propiedata.com>
2025-01-17 13:15:02 -0500

    Removed the use of Meteor.startup to init package

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
-Meteor.startup(() => {
-  try {
-    if (require("zod")) {
-      import "./src/extendWithSchema";
-    }
-  } catch (e) {
-    console.error("zod package not found. Without it, you can't use zod-schema package.");
-    console.error("Please install zod using meteor npm install zod\n\n");
-  
-    console.error(e);
-  }  
-});
+try {
+  if (require("zod")) {
+    import "./src/extendWithSchema";
+  }
+} catch (e) {
+  console.error("zod package not found. Without it, you can't use zod-schema package.");
+  console.error("Please install zod using meteor npm install zod\n\n");
+
+  console.error(e);
+}
