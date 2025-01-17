2ac28ce5315305e596386e0e9cd5523a643d57aa
Paulo Mogollon <pmogollon@propiedata.com>
2025-01-17 12:59:05 -0500

    Extend Mongo on meteor startup fixed #1

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
-try {
-  if (require("zod")) {
-    import "./src/extendWithSchema";
-  }
-} catch {
-  console.log("zod package not found. Without it you can't use zod-schema package.");
-  console.log("Please install zod using meteor npm install zod");
-}
+Meteor.startup(() => {
+  try {
+    if (require("zod")) {
+      import "./src/extendWithSchema";
+    }
+  } catch (e) {
+    console.error("zod package not found. Without it, you can't use zod-schema package.");
+    console.error("Please install zod using meteor npm install zod\n\n");
+  
+    console.error(e);
+  }  
+});
